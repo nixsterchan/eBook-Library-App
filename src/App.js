67ac36dc9e32b3fa53321fe6fc860c5575fe9772
nixsterchan <nigel_chan@mymail.sutd.edu.sg>
2018-09-13 18:31:29 +0800

    Added Login and Homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
+// App will serve as a Route Dispatcher
+// ie, basically it renders things dependent on the route
 
-const App = () => <div>App</div>
+const App = () => 
+  <div>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
+
 export default App;
